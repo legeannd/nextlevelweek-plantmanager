63471d5ba65af5049b4f7ba13f3e0ca9be922638
Gean Lucas <gean.lfpsa@gmail.com>
2021-04-19 22:05:47 -0300

    Created welcome page

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import colors from '../styles/colors';
+
+export function Button() {
+  return (
+    <TouchableOpacity 
+      style={styles.button} 
+      activeOpacity={0.7}
+    >
+      <Text style={styles.buttonText}>
+        >
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
+  }
+})
