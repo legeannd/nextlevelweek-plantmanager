408f9558e10c2b12ca4ae39a7768471c71aaaf58
Gean Lucas <gean.lfpsa@gmail.com>
2021-04-22 20:14:34 -0300

    Flatlist component id refactoring

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -43,7 +43,6 @@ export function PlantSelect () {
   
   const [page, setPage] = useState(1);
   const [loadingMore, setLoadingMore] = useState(false);
-  const [loadedAll, setLoadedAll] = useState(false);
 
   function handleEnvironmentSelected(environment: string) {
     setEnvironmentSelected(environment);
@@ -123,6 +122,7 @@ export function PlantSelect () {
       <View>
         <FlatList 
           data={environments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({ item }) => (
             <EnvironmentButton 
               title={item.title}
@@ -139,6 +139,7 @@ export function PlantSelect () {
       <View style={styles.plants}>
         <FlatList
           data={filteredPlants}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
             <PlantCardPrimary data={item} />
           )}
