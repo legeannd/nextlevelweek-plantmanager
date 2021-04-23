25260662aa5f6bf7ed529243521ab7fbbeacec23
Gean Lucas <gean.lfpsa@gmail.com>
2021-04-22 22:29:06 -0300

    Added tabs navigation to work with stack navigation

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -9,6 +9,7 @@ import { PlantSelect } from '../pages/PlantSelect';
 import colors from '../styles/colors';
 import { PlantSave } from '../pages/PlantSave';
 import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -38,7 +39,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen 
       name="PlantSelect"
-      component={PlantSelect}
+      component={AuthRoutes}
     />
 
     <stackRoutes.Screen 
@@ -48,7 +49,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen 
       name="MyPlants"
-      component={MyPlants}
+      component={AuthRoutes}
     />
   </stackRoutes.Navigator>
 )
